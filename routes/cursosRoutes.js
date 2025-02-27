d2df195ef9b75ae159622b1b04083b09283bb4cf
Alejandro <alejandro_gonzalo92@icloud.com>
2025-02-27 16:54:23 +0100

    cursosRoutes
    se crea cursosRoutes.js

---
+++ routes/cursosRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const cursosController = require('../controllers/cursosController.js');
+
+router.get('/',cursosController.consultar);
+router.post('/', cursosController.ingresar);
+
+router.route("/:id")
+    .get(cursosController.consultarDetalle)
+    .put(cursosController.actualizar)
+    .delete(cursosController.borrar);
+
+
+
+module.exports = router;    
